a1b64742170620959942e4ea49a6a48c70486747
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2020-06-19 12:34:09 +0200

    first commit!

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
+        110: '28rem',
+        132: '35rem',
+        168: '42rem',
+        182: '48rem',
+      },
+    },
+
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
+  plugins: [],
+}
