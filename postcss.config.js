2db158a43f8e36456334133a364e2204efc058a3
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2020-06-19 13:02:09 +0200

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,11 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './public/index.html'
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
     ]
 }
