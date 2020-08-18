333f6b7fda024fffb3d779c7423a45184b53c607
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2020-08-18 21:40:48 +0200

    better css

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,12 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        /*
+
         require('@fullhuman/postcss-purgecss')({
             content: [
                 './public/index.html'
             ],
             defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        })*/
+        })
     ]
 }
